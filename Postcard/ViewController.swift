c091749e26d1d01de316d66baff92c6eb6dfc433
Xin Dong <d.xin@hotmail.com>
2014-10-22 11:21:04 -0500

    Added some UI elements

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.hidden = false
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
     }
 
 
